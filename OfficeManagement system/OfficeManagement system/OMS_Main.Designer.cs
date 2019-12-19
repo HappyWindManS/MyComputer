8f79f1e18f4cea4bad8e0609dd4baf089b0a78b0
仁 蒙 <3066004354@qq.com>
2019-12-19 14:03:29 +0800

    OMS

--- OfficeManagement system/OfficeManagement system/OMS_Main.Designer.cs
+++ OfficeManagement system/OfficeManagement system/OMS_Main.Designer.cs
@@ -28,12 +28,173 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.EmployeeManage = new System.Windows.Forms.TabPage();
+            this.DocumentLibrary = new System.Windows.Forms.TabPage();
+            this.AttendanceManage = new System.Windows.Forms.TabPage();
+            this.FileManage = new System.Windows.Forms.TabPage();
+            this.StaffManage = new System.Windows.Forms.TabPage();
+            this.Affair = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.EmployeeManage.SuspendLayout();
+            this.DocumentLibrary.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.EmployeeManage);
+            this.tabControl1.Controls.Add(this.DocumentLibrary);
+            this.tabControl1.Controls.Add(this.AttendanceManage);
+            this.tabControl1.Controls.Add(this.FileManage);
+            this.tabControl1.Controls.Add(this.StaffManage);
+            this.tabControl1.Controls.Add(this.Affair);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(845, 496);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // EmployeeManage
+            // 
+            this.EmployeeManage.Controls.Add(this.label4);
+            this.EmployeeManage.Controls.Add(this.label3);
+            this.EmployeeManage.Controls.Add(this.label2);
+            this.EmployeeManage.Controls.Add(this.label1);
+            this.EmployeeManage.Location = new System.Drawing.Point(4, 25);
+            this.EmployeeManage.Name = "EmployeeManage";
+            this.EmployeeManage.Padding = new System.Windows.Forms.Padding(3);
+            this.EmployeeManage.Size = new System.Drawing.Size(837, 467);
+            this.EmployeeManage.TabIndex = 0;
+            this.EmployeeManage.Text = "个人信息";
+            this.EmployeeManage.UseVisualStyleBackColor = true;
+            // 
+            // DocumentLibrary
+            // 
+            this.DocumentLibrary.Controls.Add(this.label5);
+            this.DocumentLibrary.Location = new System.Drawing.Point(4, 25);
+            this.DocumentLibrary.Name = "DocumentLibrary";
+            this.DocumentLibrary.Padding = new System.Windows.Forms.Padding(3);
+            this.DocumentLibrary.Size = new System.Drawing.Size(837, 467);
+            this.DocumentLibrary.TabIndex = 1;
+            this.DocumentLibrary.Text = "文件借阅";
+            this.DocumentLibrary.UseVisualStyleBackColor = true;
+            // 
+            // AttendanceManage
+            // 
+            this.AttendanceManage.Location = new System.Drawing.Point(4, 25);
+            this.AttendanceManage.Name = "AttendanceManage";
+            this.AttendanceManage.Size = new System.Drawing.Size(837, 467);
+            this.AttendanceManage.TabIndex = 2;
+            this.AttendanceManage.Text = "考勤管理";
+            this.AttendanceManage.UseVisualStyleBackColor = true;
+            // 
+            // FileManage
+            // 
+            this.FileManage.Location = new System.Drawing.Point(4, 25);
+            this.FileManage.Name = "FileManage";
+            this.FileManage.Size = new System.Drawing.Size(837, 467);
+            this.FileManage.TabIndex = 3;
+            this.FileManage.Text = "文件管理";
+            this.FileManage.UseVisualStyleBackColor = true;
+            // 
+            // StaffManage
+            // 
+            this.StaffManage.Location = new System.Drawing.Point(4, 25);
+            this.StaffManage.Name = "StaffManage";
+            this.StaffManage.Size = new System.Drawing.Size(837, 467);
+            this.StaffManage.TabIndex = 4;
+            this.StaffManage.Text = "员工管理";
+            this.StaffManage.UseVisualStyleBackColor = true;
+            // 
+            // Affair
+            // 
+            this.Affair.Location = new System.Drawing.Point(4, 25);
+            this.Affair.Name = "Affair";
+            this.Affair.Size = new System.Drawing.Size(837, 467);
+            this.Affair.TabIndex = 5;
+            this.Affair.Text = "事务";
+            this.Affair.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "姓名：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "性别：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "简介：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "年龄：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(43, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 15);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "label5";
+            // 
+            // OMS_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "OMG_Main";
+            this.ClientSize = new System.Drawing.Size(737, 458);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "OMS_Main";
+            this.Text = "OMS_Main";
+            this.tabControl1.ResumeLayout(false);
+            this.EmployeeManage.ResumeLayout(false);
+            this.EmployeeManage.PerformLayout();
+            this.DocumentLibrary.ResumeLayout(false);
+            this.DocumentLibrary.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage EmployeeManage;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage DocumentLibrary;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TabPage AttendanceManage;
+        private System.Windows.Forms.TabPage FileManage;
+        private System.Windows.Forms.TabPage StaffManage;
+        private System.Windows.Forms.TabPage Affair;
     }
 }
