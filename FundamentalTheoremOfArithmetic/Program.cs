874783a7b4e45f85d90b318e5fe26acef165b79b
仁 蒙 <3066004354@qq.com>
2020-01-14 11:19:08 +0800

    math

---
+++ FundamentalTheoremOfArithmetic/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+namespace FundamentalTheoremOfArithmetic
+{
+    class Program
+    {
+        public Stack stack = new Stack();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("唯一析因定理是一个有趣的东西");
+            Console.WriteLine("它能将一个分解为一个又一个质数");
+            Console.WriteLine("这是初等数论的内容");
+            Console.WriteLine("比如，");
+        }
+
+        public void Resolve()
+        {
+
+        }
+    }
+}
