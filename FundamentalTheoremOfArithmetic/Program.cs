6bd6b0c05b0870bca4a672e11a524dd34e5926fe
仁 蒙 <3066004354@qq.com>
2020-01-14 17:53:08 +0800

    math

--- FundamentalTheoremOfArithmetic/Program.cs
+++ FundamentalTheoremOfArithmetic/Program.cs
@@ -1,47 +1,63 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Text;
 
 namespace FundamentalTheoremOfArithmetic
 {
     class Program
     {
-        public Stack stack = new Stack();
-        public static Stack prime = new Stack();
+        public int[] stack = new int[2];
+        public static List<int> prime = new List<int>();
+        public static StringBuilder stringBuilder = new StringBuilder();
         static void Main(string[] args)
         {
             GetPrime();
             Console.WriteLine("唯一析因定理是一个有趣的东西");
-            Console.WriteLine("它能将一个分解为一个又一个质数");
-            Console.WriteLine("这是初等数论的内容");
+            Console.WriteLine("它能将一个合数分解为一个又一个质数");
+            Console.WriteLine("这是数论的内容");
             Console.WriteLine("比如，");
+            Resolve(9, prime);
+            Console.WriteLine(stringBuilder);
+            Console.ReadLine();
         }
 
         public static void GetPrime()
         {
-            for(int i=0;i<10000;i++)
+            for(int i=0;i<100000;i++)
             {
-                for (int j = 2; j < i/2; j++)
+                for (int j = 2; j < i/2+1; j++)
                 {
                     //当 i 可以被 j 整除时跳出该循环,该数不是素数,不做输出
                     if (i % j == 0)
                     {
-                        prime.Push(j);
+                        if (!prime.Contains(j))
+                        {
+                            prime.Add(j);
+                        }
                         break;
                     }
                 }
             }
         }
-
-        public int Resolve(int num)
+        public static int Resolve(int num, List<int> prime)
         {
-            if(prime.Contains(num))
+            if (prime.Contains(num)||num==1)
             {
+                stringBuilder.Append(num.ToString());
                 return num;
             }
             else
             {
-
-                return Resolve(num);
+                foreach (var a in prime)
+                {
+                    if (num % a == 0)
+                    {
+                        num = num / a;
+                        stringBuilder.Append(a.ToString() + "*");
+                    }
+                }
+                return Resolve(num,prime);
             }
         }
     }
