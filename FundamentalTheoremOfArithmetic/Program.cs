625496dcbdd87d883aaa3df07b796261f980826b
仁 蒙 <3066004354@qq.com>
2020-01-16 18:04:08 +0800

    prime

--- FundamentalTheoremOfArithmetic/Program.cs
+++ FundamentalTheoremOfArithmetic/Program.cs
@@ -10,54 +10,55 @@ namespace FundamentalTheoremOfArithmetic
         public int[] stack = new int[2];
         public static List<int> prime = new List<int>();
         public static StringBuilder stringBuilder = new StringBuilder();
+        public static Hashtable hashtable = new Hashtable();
         static void Main(string[] args)
         {
-            GetPrime();
             Console.WriteLine("唯一析因定理是一个有趣的东西");
             Console.WriteLine("它能将一个合数分解为一个又一个质数");
             Console.WriteLine("这是数论的内容");
             Console.WriteLine("比如，");
-            Resolve(9, prime);
-            Console.WriteLine(stringBuilder);
+            Resolve(864531);
+            foreach(var str in hashtable.Keys)
+            {
+                Console.WriteLine("{0}^{1}",str,hashtable[str]);
+            }
             Console.ReadLine();
         }
 
-        public static void GetPrime()
+        /// <summary>
+        /// 判断素数
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static int BoolGetPrime(int num)
         {
-            for(int i=0;i<100000;i++)
+            for (int j = 2; j < num/2; j++)
             {
-                for (int j = 2; j < i/2+1; j++)
+                //当 i 可以被 j 整除时跳出该循环,该数不是素数,不做输出
+                if (num % j == 0)
                 {
-                    //当 i 可以被 j 整除时跳出该循环,该数不是素数,不做输出
-                    if (i % j == 0)
-                    {
-                        if (!prime.Contains(j))
-                        {
-                            prime.Add(j);
-                        }
-                        break;
-                    }
-                }
+                    return j;
+                }             
             }
+            return 0;
         }
-        public static int Resolve(int num, List<int> prime)
+        public static int Resolve(int num)
         {
-            if (prime.Contains(num)||num==1)
+            int m = BoolGetPrime(num);
+            if (hashtable.Contains(m))
             {
-                stringBuilder.Append(num.ToString());
+                hashtable[m] = (int)hashtable[m] + 1;
+                return Resolve(num / m);           
+            }
+            else if (m == 0)
+            {
+                hashtable.Add(num, 1);
                 return num;
             }
-            else
+            else 
             {
-                foreach (var a in prime)
-                {
-                    if (num % a == 0)
-                    {
-                        num = num / a;
-                        stringBuilder.Append(a.ToString() + "*");
-                    }
-                }
-                return Resolve(num,prime);
+                hashtable.Add(m, 1);
+                return Resolve(num/m);
             }
         }
     }
